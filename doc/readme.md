b9fc51dd05cfa76b41377be7ecafbe9bc19aa3b4
Larkin <la2n@qq.com>
2016-06-01 17:32:45 +0800

    init
    init

---
+++ doc/readme.md
@@ -0,0 +1,128 @@
+# realClock.js - 获取实时时间控件
+
+### 特性
+
+- 单体控件，弃置了OO（new XXX）的使用方式，杜绝旧控件的以下问题：
+
+    + 重复使用造成性能损耗乃至内存泄露
+
+    + 诡异的多处时间不同步
+
+    + 不必要的客户端和服务器压力
+
+- API支持需求定制，demo集成了全球时间、倒计时等常用业务
+
+- 业务调用返回一个```时间task```，用于暂停，销毁等后续操作
+
+------------
+
+### 资源引用
+
+``` html
+<script src="src/realClock.js"></script>
+```
+
+### 方法
+
+- getStatus() : 获取控件状态
+
+- getTimesamp() : 获取当前时间戳
+
+- bindView(conf[Object]) : 绑定视图更新，返回对应的task对象
+
+``` javascript
+// bindView conf格式示例
+{
+    node : document.getElementById('time_app'),  // 注入的视图节点
+    template : '现在是：#year#年#month2#月#date2#日 #hour2#点#minute2#分#second2#秒'  // 文字模板，用"#"成对引用时间数据，可选的时间数据有"year, month, date, hour, minute, second, month2（固定占双字符，如为个位数将自动在前面补0，下同）, date2, hour2, minute2, second2",
+    offset : 0  // 可选，与北京时间的偏移量，以ms为单位
+}
+```
+
+- onSecChanged(conf[Object]) : 秒变hook，返回对应的task对象，一般用于复杂需求
+
+``` javascript
+// onSecChanged conf格式示例
+{
+    listener : function (realTimesamp) {
+        // body
+    },   // 秒变hook函数，接受当前时间戳为参数
+    offset : 0  // 可选，与北京时间的偏移量，以ms为单位
+}
+```
+
+- offTask(task[Object]) : 解除特定的某个时间task
+
+`task必须为特定的task对象引用，传入匿名对象没有任何意义，下同`
+
+- pause(task[Object]) : 暂停特定的某个时间task
+
+- continue(task[Object]) : （暂停后）继续特定的某个时间task
+
+------------
+
+### 示例
+
+[demo](../index.html)
+
+##### 示例1：北京时间
+
+html:
+``` html
+<span id="time_w0"></span>
+```
+
+js:
+``` javascript
+// 绑定视图更新
+// 该行为返回一个“时间task”
+// 尽可能创建一个指向当前时间task的引用，方便后续管理
+var app0 = realClock.bindView({
+    node : document.getElementById('time_w0'),
+    template : '#year#年#month2#月#date2#日 #hour2#点#minute2#分#second2#秒'
+});
+
+// 暂停
+// realClock.pause(app0);
+
+// 暂停后继续
+// realClock.pause(app0);
+
+// 不再需要该task时，手动移除是个好习惯
+// realClock.offTask(app0);
+```
+
+##### 示例2：复杂需求定制 - 倒计时
+
+html:
+``` html
+距离2020年3月29日18点30分还有<span id="time_w6"></span>
+```
+
+js:
+``` javascript
+// 设定时间task
+var pointTask = {
+    listener : function (realTimesamp) {
+        var SECOND = 1000,
+            MINUTE = 60 * SECOND,
+            HOUR = MINUTE * 60,
+            DAY = 24 * HOUR,
+            dateOffset = 1585477800000 - realClock.getTimesamp(),
+            day, hour, minute, second;
+        if(dateOffset < 0) {
+            day = hour = minute = second = '0';
+        } else {
+            day = parseInt(dateOffset / DAY, 10);
+            hour = parseInt((dateOffset % DAY) / HOUR, 10);
+            minute = parseInt((dateOffset % HOUR) / MINUTE, 10);
+            second = parseInt((dateOffset % MINUTE) / SECOND, 10);
+        }
+        document.getElementById('time_w6').innerHTML = day + '天' + hour + '小时' + minute + '分钟' + second + '秒';
+    }
+};
+// 启动倒计时
+var app6 = realClock.onSecChanged(pointTask);
+```
+
+------------
---
+++ js/realClock.js
@@ -0,0 +1,157 @@
+var realClock = function() {
+    if (window.realClock) {
+        return realClock;
+    }
+    var verifyUrl = location.protocol.indexOf('http') > -1 ? location.protocol : 'http:' + '//hq.sinajs.cn/?rn=$random&list=sys_time',
+        _verified = false,
+        _started = false,
+        _bindTask = [],
+        _interval = 1000,
+        _maxOffset = 5 * 1000,
+        _offset,
+        _loopObj,
+        localTime;
+
+    function _start() {
+        localTime = new Date();
+        _requestReal();
+        _loop();
+        _loopObj = setInterval(_loop, _interval);
+        _started = true;
+    }
+
+    function _loop() {
+        for (var i = 0; i < _bindTask.length; i++) {
+            _bindTask[i].isActive && _bindTask[i].listener(_bindTask[i].offset ? localTime.getTime() + _bindTask[i].offset : localTime.getTime());
+        }
+        if (typeof _offset === 'number' && _offset - (localTime - (new Date())) > _maxOffset) {
+            _verified = false;
+            _requestReal();
+        }
+        localTime = new Date(localTime.getTime() + _interval);
+    }
+
+    function _requestReal() {
+        _getScript(verifyUrl.replace('$random', Math.random()), function() {
+            if (window.hq_str_sys_time) {
+                localTime = new Date(hq_str_sys_time * 1000);
+                _offset = localTime - (new Date());
+                _loop();
+                _loopObj && clearInterval(_loopObj);
+                _loopObj = setInterval(_loop, _interval);
+                _verified = true;
+            }
+        });
+    }
+
+    function _format(timestamp) {
+        var tempDate = new Date(timestamp),
+            year = tempDate.getFullYear(),
+            month = tempDate.getMonth() + 1,
+            date = tempDate.getDate(),
+            hour = tempDate.getHours(),
+            minute = tempDate.getMinutes(),
+            second = tempDate.getSeconds();
+        return {
+            year: year,
+            month: month,
+            date: date,
+            hour: hour,
+            minute: minute,
+            second: second,
+            month2: month > 9 ? month.toString() : '0' + month,
+            date2: date > 9 ? date.toString() : '0' + date,
+            hour2: hour > 9 ? hour.toString() : '0' + hour,
+            minute2: minute > 9 ? minute.toString() : '0' + minute,
+            second2: second > 9 ? second.toString() : '0' + second
+        };
+    }
+
+    function _getScript(url, dispose) {
+        var script = document.createElement('script');
+        script.type = 'text/javascript';
+        script.defer = true;
+        script.TimeOutObj = setTimeout(function() {
+            script.parentNode.removeChild(script);
+        }, 1000);
+        script[script.onreadystatechange === null ? 'onreadystatechange' : 'onload'] = function() {
+            if (this.onreadystatechange) {
+                if (this.readyState !== 'loaded' && this.readyState !== 'complete') {
+                    return;
+                }
+            }
+            if (dispose) {
+                setTimeout(dispose, 17);
+            }
+            this[this.onreadystatechange ? 'onreadystatechange' : 'onload'] = null;
+            clearTimeout(script.TimeOutObj);
+            setTimeout(function() {
+                script.parentNode.removeChild(script);
+            }, 1000);
+        };
+        script.src = url;
+        document.getElementsByTagName('head')[0].appendChild(script);
+    }
+    return {
+        getStatus: function() {
+            return {
+                started: _started,
+                verified: _verified,
+                taskLen: _bindTask.length
+            };
+        },
+        getTimesamp: function() {
+            return localTime;
+        },
+        bindView: function(conf) {
+            var task = {
+                isActive: true,
+                listener: function(realTimesamp) {
+                    var realObj = _format(realTimesamp),
+                        formatStr = conf.template
+                        .replace(/<script[^>]*?>[^<]*<\/.*?script.*?>/ig, '')
+                        .replace(/#year#/ig, realObj.year)
+                        .replace(/#month2#/ig, realObj.month2)
+                        .replace(/#month#/ig, realObj.month)
+                        .replace(/#date2#/ig, realObj.date2)
+                        .replace(/#date#/ig, realObj.date)
+                        .replace(/#hour2#/ig, realObj.hour2)
+                        .replace(/#hour#/ig, realObj.hour)
+                        .replace(/#minute2#/ig, realObj.minute2)
+                        .replace(/#minute#/ig, realObj.minute2)
+                        .replace(/#second2#/ig, realObj.second2)
+                        .replace(/#second#/ig, realObj.second);
+                    conf.node.innerHTML = formatStr;
+                },
+                offset: conf.offset
+            };
+            _bindTask.push(task);
+            !_started && _start();
+            return task;
+        },
+        onSecChanged: function(conf) {
+            var task = {
+                isActive: true,
+                listener: conf.listener,
+                offset: conf.offset
+            };
+            _bindTask.push(task);
+            !_started && _start();
+            return task;
+        },
+        offTask: function(task) {
+            var taskLen = _bindTask.length;
+            for (var i = 0; i < taskLen; i++) {
+                if (_bindTask[i] === task) {
+                    _bindTask.splice(i, 1);
+                }
+            }
+        },
+        pause: function(task) {
+            task.isActive && (task.isActive = false);
+        },
+        continue: function(task) {
+            !task.isActive && (task.isActive = true);
+        }
+    };
+}();
